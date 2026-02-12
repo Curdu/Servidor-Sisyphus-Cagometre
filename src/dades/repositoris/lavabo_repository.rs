119ed83fe4a0d8d2b597f516600bfdee417853d5
Curdu <curdu01@gmail.com>
2026-02-12 15:11:33 +0100

    Convertidas todas las fuciones de los traits del controlador, repositorio i servicio de lavabo a metodos

--- src/dades/repositoris/lavabo_repository.rs
+++ src/dades/repositoris/lavabo_repository.rs
@@ -5,9 +5,9 @@ use crate::{dades::models::lavabo::Lavabo, errors::lavabo_errors::LavaboErrors};
 
 #[async_trait]
 pub(crate) trait LavaboRepository: Sync + Send {
-    async fn obte_lavabo_per_id(id: Uuid) -> Result<Lavabo, LavaboErrors>;
-    async fn crear_lavabo(lavabo : Lavabo) -> Result<Lavabo, LavaboErrors>;
-    async fn actualitzar_lavabo(id: Uuid, lavabo : Lavabo) -> Result<Lavabo, LavaboErrors>;
-    async fn eliminar_lavabo(id: Uuid) -> Result<(), LavaboErrors>;
+    async fn obte_lavabo_per_id(&self,id: Uuid) -> Result<Lavabo, LavaboErrors>;
+    async fn crear_lavabo(&self,lavabo : Lavabo) -> Result<Lavabo, LavaboErrors>;
+    async fn actualitzar_lavabo(&self,id: Uuid, lavabo : Lavabo) -> Result<Lavabo, LavaboErrors>;
+    async fn eliminar_lavabo(&self,id: Uuid) -> Result<(), LavaboErrors>;
     
 }
--- src/serveis/lavabo_service.rs
+++ src/serveis/lavabo_service.rs
@@ -5,8 +5,8 @@ use crate::{errors::lavabo_errors::LavaboErrors, serveis::dtos::lavabo_dto::Lava
 
 #[async_trait]
 pub(crate) trait LavaboService: Sync + Send {
-    async fn crear_lavabo(lavabo_dto: LavaboDTO) -> Result<LavaboDTO, LavaboErrors>;
-    async fn obte_lavabo_per_id(id: Uuid) -> Result<LavaboDTO, LavaboErrors>;
-    async fn actualitzar_lavabo(id: Uuid, lavabo_dto: LavaboDTO) -> Result<LavaboDTO, LavaboErrors>;
-    async fn eliminar_lavabo(id: Uuid) -> Result<(), LavaboErrors>;
+    async fn crear_lavabo(&self ,lavabo_dto: LavaboDTO) -> Result<LavaboDTO, LavaboErrors>;
+    async fn obte_lavabo_per_id(&self, id: Uuid) -> Result<LavaboDTO, LavaboErrors>;
+    async fn actualitzar_lavabo(&self, id: Uuid, lavabo_dto: LavaboDTO) -> Result<LavaboDTO, LavaboErrors>;
+    async fn eliminar_lavabo(&self, id: Uuid) -> Result<(), LavaboErrors>;
 }
