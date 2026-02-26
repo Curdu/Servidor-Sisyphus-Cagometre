4ddaafaad37f2319de286608b5de9ed7600427db
Curdu <curdu01@gmail.com>
2026-02-26 12:59:05 +0100

    Creada la nova funció verificar_token

--- src/routes/auth_routes.rs
+++ src/routes/auth_routes.rs
@@ -1,10 +1,13 @@
 use std::sync::Arc;
 
-use axum::{Json, Router, extract::State, http::StatusCode, response::{IntoResponse, Response}, routing::post};
+use axum::{Json, Router, extract::{Request, State}, http::StatusCode, middleware::Next, response::{IntoResponse, Response}, routing::post};
+use axum_extra::TypedHeader;
 use chrono::{Utc};
+use headers::{Authorization, authorization::Bearer};
+use jsonwebtoken::{DecodingKey, Validation, decode};
 use uuid::Uuid;
 
-use crate::{controladors::auth_controller::AuthController, dades::models::rols::UsuariRol, errors::{auth_errors::AuthError, usuari_errors::UsuariErrors}, serveis::dtos::{auth_dto::AuthToken, usuari_dto::UsuariDTO}};
+use crate::{controladors::auth_controller::AuthController, dades::models::rols::UsuariRol, errors::{auth_errors::AuthError, usuari_errors::UsuariErrors}, routes::extractors::auth_extractors::ClaimsInfo, serveis::dtos::{auth_dto::AuthToken, usuari_dto::UsuariDTO}, state::SECRET_KEY};
 
 use super::extractors::{auth_extractors::LoginRequest, usuari_extractors::CrearUsuariRequest};
 
@@ -35,4 +38,29 @@ async fn registre(State(auth_controller) : State<Arc<dyn AuthController>>, body 
         },
         Err(error) => Err(error)
     }
+}
+
+pub (crate) async fn verificar_token(auth_header: Option<TypedHeader<Authorization<Bearer>>>, mut req: Request, next: Next) -> Result<Response, AuthError>{
+
+    let TypedHeader(auth) = auth_header.ok_or(AuthError::TokenFaltant("Falta token en la petició".to_string()))?;
+
+    let token_decodificada = decode::<ClaimsInfo>(
+        auth.token(), 
+        &DecodingKey::from_secret(SECRET_KEY.as_bytes()), 
+        &Validation::default()
+    );
+
+    let tokn = match token_decodificada {
+        Ok(token) => {
+            token.claims
+        },
+        Err(error) => {
+            return Err(AuthError::TokenInvalida(error.to_string()))
+        }
+    };
+
+    req.extensions_mut().insert(tokn);
+
+    Ok(next.run(req).await)
+
 }
