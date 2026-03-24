f2a612396a63e9b19990df7727264f8191ff26e6
Curdu <curdu01@gmail.com>
2026-03-24 21:44:24 +0100

    Afegides les rutes i controladors de etiqueta i resenya a la aplicació

--- src/container.rs
+++ src/container.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use crate::{controladors::{auth_controller::AuthControlador, lavabo_controller::LavaboControlador, user_controller::UserControlador}, dades::repositoris::postgres::{postgres_lavabo_repository::PostgresLavaboRepository, postgres_user_repository::PostgresUserRepository}, routes::Controladors, serveis::{auth_service::{AuthServei}, lavabo_service::LavaboServei, user_service::UserServei}};
+use crate::{controladors::{auth_controller::AuthControlador, etiqueta_controller::{EtiquetaControlador}, lavabo_controller::LavaboControlador, resenya_controller::ResenyaControlador, user_controller::UserControlador}, dades::repositoris::postgres::{postgres_etiqueta_repository::PostgresEtiquetaRepository, postgres_lavabo_repository::PostgresLavaboRepository, postgres_resenya_repository::PostgresResenyaRepository, postgres_user_repository::PostgresUserRepository}, routes::Controladors, serveis::{auth_service::AuthServei, etiqueta_service::EtiquetaServei, lavabo_service::LavaboServei, resenya_service::ResenyaServei, user_service::UserServei}};
 use sqlx::{ postgres::PgPoolOptions};
 
 pub(crate) async fn obtenir_controladors(type_bbdd : String, url_bbdd : String) -> Controladors {
@@ -19,11 +19,20 @@ pub(crate) async fn obtenir_controladors(type_bbdd : String, url_bbdd : String) 
 
         let auth_service = AuthServei::new(usuari_repository.clone());
 
-        let user_controller = UserControlador::new(Arc::new(usuari_service));
-        let lavabo_controller = LavaboControlador::new(Arc::new(lavabo_service));
-        let auth_controller = AuthControlador::new(Arc::new(auth_service));
+        let resenya_repository = Arc::new(PostgresResenyaRepository::new(pool.clone()));
+        let resenya_service = Arc::new(ResenyaServei::new(resenya_repository));
+
+        let etiqueta_repository = Arc::new(PostgresEtiquetaRepository::new(pool.clone()));
+        let etiqueta_service = Arc::new(EtiquetaServei::new(etiqueta_repository));
+
+
+        let user_controller = Arc::new(UserControlador::new(Arc::new(usuari_service)));
+        let lavabo_controller = Arc::new(LavaboControlador::new(Arc::new(lavabo_service)));
+        let auth_controller = Arc::new(AuthControlador::new(Arc::new(auth_service)));
+        let resenya_controller = Arc::new(ResenyaControlador::new(resenya_service));
+        let etiqueta_controller = Arc::new(EtiquetaControlador::new(etiqueta_service));
         
-        Controladors{usuari : Arc::new(user_controller), lavabo : Arc::new(lavabo_controller), auth: Arc::new(auth_controller)}
+        Controladors{usuari : user_controller, lavabo : lavabo_controller, auth: auth_controller, resenya: resenya_controller, etiqueta: etiqueta_controller}
 
     }else {
         panic!("Aquesta base de dades no esta suportada")
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,15 +2,19 @@ use std::sync::Arc;
 
 use auth_routes::{get_auth_router, verificar_token};
 use axum::{Router, http::Method, middleware};
+use etiqueta_routes::get_etiqueta_router;
 use lavabo_routes::get_lavabo_router;
+use resenya_routes::get_resenya_router;
 use tower_http::cors::{Any, CorsLayer};
 
-use crate::{controladors::{auth_controller::AuthController, lavabo_controller::LavaboController, user_controller::UserController}, routes::user_routes::get_user_router};
+use crate::{controladors::{auth_controller::AuthController, icontroller::IController, lavabo_controller::LavaboController, user_controller::UserController}, routes::user_routes::get_user_router, serveis::dtos::{etiqueta_dto::EtiquetaDTO, resenya_dto::ResenyaDTO}};
 
 pub(crate) mod user_routes;
 pub(crate) mod extractors;
 pub(crate) mod lavabo_routes;
 pub(crate) mod auth_routes;
+pub(crate) mod etiqueta_routes;
+pub(crate) mod resenya_routes;
 
 pub(crate) fn get_router(controladors: Controladors) -> Router {
 
@@ -19,13 +23,18 @@ pub(crate) fn get_router(controladors: Controladors) -> Router {
         .allow_methods([Method::GET, Method::POST,Method::PUT,Method::DELETE])
         .allow_headers(Any);
 
-    let usuari_router = get_user_router(controladors.usuari).route_layer(middleware::from_fn(verificar_token));
+    let middleware_verificacio = middleware::from_fn(verificar_token);
+    let usuari_router = get_user_router(controladors.usuari).route_layer(middleware_verificacio.clone());
     let lavabo_router = get_lavabo_router(controladors.lavabo);
     let auth_router = get_auth_router(controladors.auth);
+    let resenya_router = get_resenya_router(controladors.resenya).route_layer(middleware_verificacio.clone());
+    let etiqueta_router = get_etiqueta_router(controladors.etiqueta).route_layer(middleware_verificacio.clone());
 
     Router::new()
         .nest("/api/usuaris", usuari_router)
         .nest("/api/lavabos", lavabo_router)
+        .nest("/api/resenyes", resenya_router)
+        .nest("/api/etiquetes", etiqueta_router)
         .nest("/api", auth_router)
         .layer(cors_layer)
 }
@@ -33,5 +42,8 @@ pub(crate) fn get_router(controladors: Controladors) -> Router {
 pub(crate) struct  Controladors {
     pub(crate) usuari: Arc<dyn UserController>,
     pub(crate) lavabo: Arc<dyn LavaboController>,
-    pub(crate) auth: Arc<dyn AuthController>
+    pub(crate) auth: Arc<dyn AuthController>,
+    pub(crate) resenya: Arc<dyn IController<ResenyaDTO>>,
+    pub(crate) etiqueta: Arc<dyn IController<EtiquetaDTO>>
 }
+
