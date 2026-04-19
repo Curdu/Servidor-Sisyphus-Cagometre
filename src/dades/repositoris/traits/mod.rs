fe68a3866581da9c2bdb6fc219c6315c843ac7df
Curdu <curdu01@gmail.com>
2026-04-19 18:14:11 +0200

    Afegit modul per afegir o eliminar imatges de buckets de supabase

--- src/dades/repositoris/traits/mod.rs
+++ src/dades/repositoris/traits/mod.rs
@@ -2,3 +2,4 @@ pub(crate) mod user_repository;
 pub(crate) mod lavabo_repository;
 pub(crate) mod etiqueta_repository;
 pub(crate) mod compartit;
+pub(crate) mod image_repository;
--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -2,3 +2,4 @@ pub(crate) mod usuari_errors;
 pub(crate) mod lavabo_errors;
 pub(crate) mod auth_errors;
 pub(crate) mod crud_errors;
+pub(crate) mod storage_errors;
